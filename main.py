65f5b990149c376c78701bd4a34fdccf34c1270c
iagalliadis <johnalias25@gmail.com>
2019-10-10 10:21:38 +0300

    Content-Based system fetching recommendations

--- main.py
+++ main.py
@@ -154,3 +154,28 @@ ax.set_ylabel('Number of Ratings', fontsize=16, labelpad=20)
 plt.scatter(ratings_df['Mean_Rating'], ratings_df['Num_Ratings'], alpha=0.5)
 
 #%%
+matrix = data.pivot_table(
+    index='userId',
+    columns='title',
+    values='rating'
+)
+
+#%%
+
+def get_similar_movies(movie_title, n_ratings_filter=100, n_recommendations=5):
+    similar = matrix.corrwith(matrix[movie_title])
+    corr_similar = pd.DataFrame(similar, columns=['correlation'])
+    corr_similar.dropna(inplace=True)
+    
+    orig = data.copy()
+    
+    corr_with_movie = pd.merge(
+        left=corr_similar, 
+        right=orig, 
+        on='title')[['title', 'correlation', 'numRatings']].drop_duplicates().reset_index(drop=True)
+    
+    result = corr_with_movie[corr_with_movie['numRatings'] > n_ratings_filter].sort_values(by='correlation', ascending=False)
+    
+    return result.head(n_recommendations)
+
+#%%
